3df5c587ce2e002445871ac8b3e516594846e6be
Brandon Bermejo <brandoxo90@gmail.com>
2025-04-02 10:27:50 -0600

    Reorder files and adding tailwind file config

---
+++ bankzar/tailwind.config.js
@@ -0,0 +1,13 @@
+    @type {import('tailwindcss').Config}
+    module.exports = {
+        darkMode: 'class',
+        content: ['./src/**/*.{html,js]'],
+        theme: {
+            extend: {
+                screens:{
+                    'tablet': '900px',
+                }
+            },
+        },
+        plugins:[],
+    }
